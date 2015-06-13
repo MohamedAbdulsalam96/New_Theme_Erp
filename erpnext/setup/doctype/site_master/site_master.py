01b8065849e69e16815ab8429eaf9d7dae7b03e4
Rohit <tejal.s@indictranstech.com>
2015-06-13 21:15:57 +0530

    custom field changes

--- erpnext/setup/doctype/site_master/site_master.py
+++ erpnext/setup/doctype/site_master/site_master.py
@@ -132,10 +132,10 @@ def validate_validity(doc, method):
 		#res =''
 	 	res = frappe.db.sql("select name,user_name,validity from `tabUser Validity` where  user_name >0",as_list=1)
 	 	if  res:
-	 			frappe.db.sql("update `tabUser Validity` set user_name=user_name-1  where name=%s" %(res[0][0]),debug=1)
+	 			frappe.db.sql("update `tabUser Validity` set user_name=user_name-1  where name='%s'" %(res[0][0]),debug=1)
 				from frappe.utils import nowdate,add_months,cint
 				doc.validity_start_date=nowdate()
-				doc.validity_end_date=add_months(nowdate(),res[0][2])
+				doc.validity_end_date=add_months(nowdate(),cint(res[0][2]))
 		else:
 			res1 = frappe.db.sql("select count(name) from `tabUser`")
 	 		if res1 and res1[0][0]==2:
